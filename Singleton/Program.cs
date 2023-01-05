0e128b78fc59c2a7911b820dfee4e4fc8867d624
Alexandre R Oliveira <alexandre.programador84@gmail.com>
2023-01-04 21:11:56 -0300

    Padrão Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Singleton");
+
+SingletonClass jogador_1 = SingletonClass.GetInstancia;
+jogador_1.Mensagem("Jogador_1 está com a bola");
+
+SingletonClass jogador_2 = SingletonClass.GetInstancia;
+jogador_2.Mensagem("Jogador_2 recebeu a bola.");
+
+SingletonClass jogador_3 = SingletonClass.GetInstancia;
+jogador_3.Mensagem("Jogador_3 recebeu o cruzamento.");
+
+Console.ReadKey();
+
+
---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton
+{
+    public sealed class SingletonClass
+    {
+        private static SingletonClass instancia = null;
+
+        public static SingletonClass GetInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new SingletonClass();
+                    Console.WriteLine("Bola em jogo");
+                }
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
